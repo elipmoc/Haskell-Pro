934105f179eff996acd09c8f765b951b03ebf051
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-11-28 15:23:33 +0900

    フィボナッチ数を求める関数をシングルとマルチで時間計測するコードできた

--- fib-para/src/Main.hs
+++ fib-para/src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import Control.Monad.Par (spawnP,get,runPar)
+import System.Environment (getArgs)
+import Data.Time.Clock
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  beginTime <- getCurrentTime
+  args<- getArgs
+  let (n:_)=fmap read args
+  print $ parallelFib n
+  endTime <- getCurrentTime
+  putStrLn "DONE"
+  putStrLn $ "Time: " ++ show (diffUTCTime endTime beginTime)
+  
+--フィボナッチ数を並列で計算する
+--Parモナドによってivar1とivar2の組、result1とresult2の組が並列に求められる
+parallelFib::Int->Int
+parallelFib 0 = 0
+parallelFib 1 = 1
+parallelFib n = runPar $ do
+  ivar1 <- spawnP $ fib (n-1)
+  ivar2 <- spawnP $ fib (n-2)
+  result1 <- get ivar1
+  result2 <- get ivar2
+  return $ result1+result2
+
+--普通のフィボナッチ数を計算する関数
+fib::Int->Int
+fib 0 =0
+fib 1 =1
+fib n = fib(n-1)+fib(n-2)
+
