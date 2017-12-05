c70b6f3564db649e02661a517e78070cd8ea2243
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-05 15:24:10 +0900

    jqFilterParserに新たにテストを追加

--- hjq/test/Spec.hs
+++ hjq/test/Spec.hs
@@ -17,5 +17,9 @@ main = do
 jqFilterParserTest :: Test
 jqFilterParserTest = TestList
     [
-        "jqParser test 1" ~: parserJqFilter "." ~?= Right JqNil -- .が来たらJqNil
+        "jqParser test 1" ~: parserJqFilter "." ~?= Right JqNil
+        ,"jqParser test 2" ~: parserJqFilter ".[0]" ~?= Right (JqIndex 0 JqNil)
+        ,"jqParser test 3" ~: parserJqFilter ".fieldName" ~?= Right (JqField "fieldName" JqNil)
+        ,"jqParser test 4" ~: parserJqFilter ".[0].fieldName" ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
+        ,"jqParser test 5" ~: parserJqFilter ".fieldName[0]" ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
     ]
